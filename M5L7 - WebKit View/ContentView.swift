e0084ceb2e21d3ee2be7f76fdd29f6769c0443e5
Ekkehard <emkinter@me.com>
2022-04-13 06:42:09 -0600

    Initial Commit

---
+++ M5L7 - WebKit View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M5L7 - WebKit View
+//
+//  Created by Ekkehard Koch on 2022.04.13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
